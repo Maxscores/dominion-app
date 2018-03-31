04c4a98ae066700242b3536620caf203cde904c4
maxscores <maxstackhouse@gmail.com>
2018-03-31 12:26:05 -0600

    Add turns/index.js

---
+++ containers/Playground/Turns/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class Table extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Turns</Text>
+      </View>
+    )
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#abcdef'
+  }
+})
