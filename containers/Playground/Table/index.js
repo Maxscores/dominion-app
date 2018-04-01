1753e87d61c8d671077c2cabaac5787cf4677280
TMad <madsen.tyler@gmail.com>
2018-04-01 17:52:26 -0600

    Add PlayArea  and Hand to Table

--- containers/Playground/Table/index.js
+++ containers/Playground/Table/index.js
@@ -7,15 +7,25 @@ import {
 import { responsiveHeight, responsiveWidth, responsiveFontSize } from 'react-native-responsive-dimensions';
 
 import Supply from './Supply';
+import Hand from './Hand';
+import PlayArea from './PlayArea';
 import Scoreboard from '../../../components/Scoreboard';
+import TurnDetail from '../../../components/TurnDetail';
 
 export default class Table extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <TurnDetail />
         <View style={styles.topContainer}>
           <Supply style={styles.supply}/>
           <Scoreboard />
+        </View>
+        <View style={styles.playContainer}>
+          <PlayArea />
+        </View>
+        <View>
+          <Hand />
         </View>
       </View>
     )
@@ -24,6 +34,9 @@ export default class Table extends Component {
 
 
 const styles = StyleSheet.create({
+  playContainer: {
+    height: responsiveWidth(60),
+  },
   container: {
     flex: 1,
   },
