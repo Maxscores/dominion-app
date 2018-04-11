29035b4eed3a04acc532531380270af02f4e5d36
maxscores <maxstackhouse@gmail.com>
2018-04-11 14:22:33 -0600

    Move Gamestate up

--- containers/Playground/index.js
+++ containers/Playground/index.js
@@ -11,6 +11,9 @@ import Table from './Table'
 import Deck from './Deck'
 import Turns from './Turns'
 
+import { getGameState, postTurn } from '../../game-utilities/services'
+
+
 const routeConfig = {
   Table: {
     screen: Table,
@@ -38,9 +41,37 @@ const navConfig = {
 const RootNav = TabNavigator(routeConfig, navConfig)
 
 export default class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			localPlayer: 1,
+			gameId: 3,
+			currentPlayer: null,
+			supply: {},
+      draw: [],
+      discard: [],
+      hand: [],
+      trash: [],
+			turnOrder: [],
+			playarea: [],
+			actions: 1,
+			buys: 1,
+			coins: 0,
+			cardsGained: [],
+			cardsTrashed: [],
+			hasBought: false,
+			competitors: [],
+			attackStack: {},
+			actionStack: [],
+			cardImage: "copperFull",
+			turnPhase: 1,
+			popupAction: null,
+		}
+	}
+
   render() {
     return (
-      <RootNav />
+      <RootNav screenProps={{state: this.state, setParentState: this.setState.bind(this)}} />
     );
   }
 }
