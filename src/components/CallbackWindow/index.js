5cdace21272e47b4293cccaed4b63d19c3636cd8
TMad <madsen.tyler@gmail.com>
2018-04-12 21:08:10 -0600

    Add chapel multiselect callback to CallbackWindow

--- src/components/CallbackWindow/index.js
+++ src/components/CallbackWindow/index.js
@@ -4,18 +4,49 @@ import {
   View,
   Text,
   Button,
+  Image,
 } from 'react-native';
 import { responsiveHeight, responsiveWidth, responsiveFontSize } from 'react-native-responsive-dimensions';
 
 import { images } from '@assets/images'
 import dominionCards from '../../game-utilities/dominion'
+import SelectMultiple from 'react-native-select-multiple'
 
+const renderLabel = (label, style) => {
+  return (
+    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+      <View style={{marginLeft: 10}}>
+        <Text style={style}>{label}</Text>
+      </View>
+    </View>
+  )
+}
 
 export default class CallbackWindow extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      selectedCards: [],
+    }
   }
+
+  checkMaxSelected(maxCount) {
+    if (this.state.selectedCards.length <= maxCount) {
+      let cards =  this.state.selectedCards.map((card) => {
+        return card.label
+      })
+      this.props.playChapel(cards)
+      this.props.resolveActionStack()
+    } else {
+      alert(`Please select ${maxCount} cards or fewer`)
+    }
+  }
+
+  onSelectedCardsChange(selectedCards) {
+    this.setState({selectedCards: selectedCards})
+  }
+
 
   cardCallbacks = {
     "vassal": () => {
@@ -49,6 +80,32 @@ export default class CallbackWindow extends Component {
           </View>
         )
       }
+    },
+    "chapel": () => {
+      if (this.props.actionStack[0].handCards.length > 0) {
+        return (
+          <View>
+          <SelectMultiple
+            items={this.props.actionStack[0].handCards}
+            renderLabel={renderLabel}
+            selectedItems={this.state.selectedCards}
+            onSelectionsChange={this.onSelectedCardsChange.bind(this)}
+          />
+          <Button
+            style={{marginBottom: 2}}
+            title="Trash"
+            onPress={
+              () => {
+                this.checkMaxSelected(4)
+              }
+            }
+          >
+          </Button>
+          </View>
+              )
+      } else {
+        () => {this.props.resolveActionStack()}
+      }
     }
   }
 
@@ -65,10 +122,17 @@ const styles = StyleSheet.create({
   overlay: {
     position: 'absolute',
     top: responsiveHeight(9),
-    bottom: 200,
+    bottom: responsiveHeight(18),
     left: 0,
     right: 0,
     borderWidth: 0,
+    paddingBottom: responsiveHeight(5),
     backgroundColor: 'orange',
   },
+  submit: {
+    backgroundColor: '#1d63d3',
+    color: 'white',
+    padding: 15,
+    marginBottom: 2
+  }
 })
