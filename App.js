2bf3c6b1efe8707b1cfd47783c6eb1a50c1f6c68
maxscores <maxstackhouse@gmail.com>
2018-04-14 17:16:21 -0600

    App only renders home

--- App.js
+++ App.js
@@ -7,24 +7,7 @@ import {
 import {
   TabNavigator
 } from 'react-navigation';
-import Playground from './src/containers/Playground'
 import Home from './src/containers/Home'
-
-// const routeConfig = {
-//   Playground: {
-//     screen: Playground,
-//   },
-// }
-//
-// const navConfig = {
-//   tabBarOptions: {
-//     labelStyle: {
-//       fontSize: 22,
-//     }
-//   }
-// }
-//
-// const RootNav = TabNavigator(routeConfig, navConfig)
 
 export default class App extends Component {
   render() {
