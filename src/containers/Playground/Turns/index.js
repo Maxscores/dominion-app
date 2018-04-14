7a34bf727870aaa478c57f505bbd8607d71ea96b
maxscores <maxstackhouse@gmail.com>
2018-04-14 17:17:39 -0600

    Remove Top Magins from Playground

--- src/containers/Playground/Turns/index.js
+++ src/containers/Playground/Turns/index.js
@@ -45,7 +45,6 @@ export default class Turns extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: responsiveHeight(4),
     flex: 1,
     textAlign: 'center',
     backgroundColor: '#abcdef'
