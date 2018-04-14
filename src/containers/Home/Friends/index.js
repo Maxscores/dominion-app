05ec998702ea3979c0219c20784aef1c3a09f2a8
maxscores <maxstackhouse@gmail.com>
2018-04-14 15:29:24 -0600

    Add Friends index.js

--- src/containers/Home/Friends/index.js
+++ src/containers/Home/Friends/index.js
@@ -2,14 +2,39 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  TextInput,
+  View,
+	Button
 } from 'react-native';
+import FriendsList from './FriendsList'
+import AddFriendForm from './AddFriendForm'
 
 export default class Friends extends Component {
+	constructor() {
+		super()
+		this.state = {
+			text: ''
+		}
+	}
+
+	friendsList() {
+		return <FriendsList friends={this.props.screenProps.friends} />
+	}
+
+	addFriendForm() {
+		return (
+			<AddFriendForm
+				onChangeMethod={(text) => {this.setState({text}).bind(this)}}
+				text={this.state.text}
+			/>
+		)
+	}
+
 	render() {
 		return (
 			<View>
-				<Text>Friends</Text>
+				{ this.addFriendForm() }
+				{ this.friendsList() }
 			</View>
 		)
 	}
