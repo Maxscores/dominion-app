437b32b9bdd84ea8915069b0b67755f6edf42774
maxscores <maxstackhouse@gmail.com>
2018-04-14 23:24:09 -0600

    Add addFriend form

--- src/containers/Home/Friends/AddFriendForm/index.js
+++ src/containers/Home/Friends/AddFriendForm/index.js
@@ -1,29 +1,83 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
-  StyleSheet,
-  Text,
-  TextInput,
   View,
-	Button
-} from 'react-native';
+  StyleSheet,
+  TouchableHighlight,
+  Text,
+ } from 'react-native'
+
+import t from 'tcomb-form-native'
+
+const Form = t.form.Form
+
+const Player = t.struct({
+  username: t.String,
+})
+
+const options = {
+  fields: {
+    username: {
+      error: 'Please enter a valid username'
+    },
+  },
+}
 
 export default class AddFriendForm extends Component {
-	constructor() {
-		super()
-	}
 
-	render() {
-		return (
-			<View>
-				<Text>Add Friend by username</Text>
-				<TextInput
-					onChangeText={this.props.onChangeMethod}
-					value={this.props.text}
-				/>
-				<Button
-					title='Find User'
-				/>
-			</View>
-		)
-	}
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+    }
+  }
+
+  onChange(value) {
+    this.setState(value)
+  }
+
+  handleSubmit = () => {
+    if (this.state.username.length === 0) {
+      alert('Please enter a username')
+    } else {
+			console.warn('send friend request')
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Form
+          ref={c => this._form = c}
+          type={Player}
+          onChange={this.onChange.bind(this)}
+          options={options}
+        />
+        <TouchableHighlight style={styles.button} onPress={this.handleSubmit} underlayColor='#99d9f4'>
+          <Text style={styles.buttonText}>Add Friend</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    padding: 20,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  }
+});
