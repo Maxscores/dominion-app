8590c92dda59c732e1a7b119fdfb3a4395ff54a5
maxscores <maxstackhouse@gmail.com>
2018-04-17 19:41:48 -0600

    adds token to user info

--- src/game-utilities/services.js
+++ src/game-utilities/services.js
@@ -81,6 +81,7 @@ const userInfoPrep = (userInfoRaw) => {
 		username: userInfoRaw.username,
 		password: userInfoRaw.password,
 		phone_number: userInfoRaw.phoneNumber,
+		token: userInfoRaw.token["_55"],
 	}
 }
 
