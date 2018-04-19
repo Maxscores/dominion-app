f6646ad39388ab6d0184a6754be7f5d073bb58de
maxscores <maxstackhouse@gmail.com>
2018-04-18 21:43:51 -0600

    Add friendship card

--- src/components/FriendshipCard/index.js
+++ src/components/FriendshipCard/index.js
@@ -7,48 +7,43 @@ import {
   TouchableHighlight,
 } from 'react-native';
 import { responsiveWidth, responsiveHeight, responsiveFontSize } from 'react-native-responsive-dimensions';
-import { getGameState, postTurn } from '../../game-utilities/services'
+import { getFriend } from '../../game-utilities/services'
 import { playerDeck } from '../../game-utilities/game-mechanics'
 import _ from 'lodash'
 import { images } from '@assets/images'
 
-export default class GameCard extends Component<Props> {
-	constructor() {
+export default class FriendshipCard extends Component<Props> {
+	constructor(props) {
 		super()
+		this.state = {
+			id: props.friend.id,
+			username: props.friend.username,
+			gamesPlayed: null,
+		}
 	}
 
 	componentDidMount() {
-
+		getFriend(this.state.id)
+			.then((response) => {
+				this.setState({
+					username: response.username,
+					gamesPlayed: response.games_played
+				})
+			})
 	}
 
 	render() {
 		return (
 			<View
 				style={styles.card}>
-				<Text>{this.props.friend.username}</Text>
-				<Text>{this.props.friend.gamesPlayed}Played: 10</Text>
-				<Text>{this.props.friend.record}Record: 10-0</Text>
+				<Text style={styles.currentPlayer}>{this.state.username}</Text>
+				<Text style={styles.text}>Games Played: {this.state.gamesPlayed}</Text>
 			</View>
 		)
 	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#bfa891',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-	innerCard: {
-		flexDirection: 'row',
-	},
-	title: {
-		fontSize: 20,
-		paddingHorizontal: responsiveWidth(5),
-		width: responsiveWidth(70),
-		borderBottomColor: 'white',
-	},
 	text: {
 		fontSize: 14
 	},
@@ -65,5 +60,6 @@ const styles = StyleSheet.create({
     borderWidth: 0.5,
     borderColor: 'white',
 		margin: responsiveHeight(1),
+		alignItems: 'center'
 	}
 });
--- src/containers/Home/Friends/AddFriendForm/index.js
+++ src/containers/Home/Friends/AddFriendForm/index.js
@@ -80,7 +80,6 @@ const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
 		alignItems: 'center',
-    padding: 20,
   },
   buttonText: {
     fontSize: 18,
