b89515183f8fcce3c9d7c80e976d6c3bf88fa471
TMad <madsen.tyler@gmail.com>
2018-04-19 09:05:22 -0600

    Position phase button right and add drop shadow

--- src/components/PhaseButton/index.js
+++ src/components/PhaseButton/index.js
@@ -3,7 +3,7 @@ import {
   StyleSheet,
   View,
   Text,
-  Button,
+  TouchableHighlight,
 } from 'react-native';
 import { responsiveHeight, responsiveWidth, responsiveFontSize } from 'react-native-responsive-dimensions';
 import { nextPhase, finishTurn } from '../../game-utilities/game-engine'
@@ -12,6 +12,10 @@ import { isActionPhase, isBuyPhase } from '../../game-utilities/game-mechanics'
 export default class PhaseButton extends Component {
 	constructor() {
 		super()
+    this.state = {
+      backgroundColor: '#2662bd',
+      borderColor: '#2662bd',
+    }
 	}
 
 	buttonTitle() {
@@ -27,6 +31,7 @@ export default class PhaseButton extends Component {
 	completePhase() {
 		if (isActionPhase(this.props.screenProps.state)) {
 			nextPhase(this.props.screenProps)
+      this.setState({backgroundColor: '#d39b17', borderColor: '#b58413'})
 		} else if (isBuyPhase(this.props.screenProps.state)) {
 			finishTurn(this.props.screenProps)
 		}
@@ -34,11 +39,41 @@ export default class PhaseButton extends Component {
 
 	render() {
 		return (
-			<Button
-			title={this.buttonTitle()}
-			onPress={ () => this.completePhase() }
-			>
-			</Button>
+      <View>
+        <TouchableHighlight
+          style={[styles.button, this.state]}
+          onPress={ () => this.completePhase() }
+          underlayColor='#99d9f4'
+        >
+          <Text style={styles.buttonText}>{this.buttonTitle()}</Text>
+        </TouchableHighlight>
+      </View>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+  buttonText: {
+    fontSize: responsiveFontSize(2),
+    color: 'white',
+    alignSelf: 'center',
+    shadowOpacity: 0,
+    elevation: 0,
+  },
+  button: {
+    height: 26,
+    borderWidth: 1,
+    borderRadius: 8,
+    marginTop: responsiveWidth(2),
+    marginRight: responsiveWidth(1),
+    marginBottom: responsiveHeight(2),
+    width: responsiveWidth(33),
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1,
+  },
+})
