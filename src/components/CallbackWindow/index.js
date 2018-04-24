0123fc76a4db23ca2c11cd8933a4da5b1deceefe
TMad <madsen.tyler@gmail.com>
2018-04-23 21:22:50 -0600

    Fix harbinger with no discard and style callback window

--- src/components/CallbackWindow/index.js
+++ src/components/CallbackWindow/index.js
@@ -3,8 +3,9 @@ import {
   StyleSheet,
   View,
   Text,
-  Button,
+  TouchableHighlight,
   Image,
+  ScrollView
 } from 'react-native';
 import { responsiveHeight, responsiveWidth, responsiveFontSize } from 'react-native-responsive-dimensions';
 
@@ -61,36 +62,47 @@ export default class CallbackWindow extends Component {
       if (dominionCards[this.props.actionQueue[0].revealedCard].type.includes('action')) {
         return(
           <View>
-            <Text>Do you want to play {this.props.actionQueue[0].revealedCard}?</Text>
-            <Button
-              title='Yes'
+            <Text
+              style={styles.text}
+            >
+              Do you want to play {this.props.actionQueue[0].revealedCard}?
+            </Text>
+            <TouchableHighlight
+              style={styles.button}
               onPress={
                 () => {
                       this.props.playVassal(this.props.actionQueue[0].revealedCard);
                       this.props.resolveActionQueue();
                       }
               }>
-            </Button>
-            <Button
-              title='No'
+                <Text style={styles.buttonText}>Yes</Text>
+            </TouchableHighlight>
+            <TouchableHighlight
+              style={styles.button}
               onPress={ () => {
 								this.props.discardVassal(1)
 								this.props.resolveActionQueue()
 							} }>
-            </Button>
+                <Text style={styles.buttonText}>No</Text>
+            </TouchableHighlight>
           </View>
         )
       } else {
         return(
           <View>
-            <Text>{this.props.actionQueue[0].revealedCard} was discarded.</Text>
-            <Button
-              title='Ok'
+            <Text
+              style={styles.text}
+            >
+              {this.props.actionQueue[0].revealedCard} was discarded.
+            </Text>
+            <TouchableHighlight
+              style={styles.button}
               onPress={ () => {
 								this.props.discardVassal(1)
 								this.props.resolveActionQueue()
 							} }>
-            </Button>
+                <Text style={styles.buttonText}>Ok</Text>
+            </TouchableHighlight>
           </View>
         )
       }
@@ -98,81 +110,111 @@ export default class CallbackWindow extends Component {
     "chapel": () => {
       if (this.props.actionQueue[0].handCards.length > 0) {
         return (
-          <View>
+          <ScrollView>
+            <Text
+              style={styles.text}
+            >
+              You may select up to 4 cards to trash from your hand.
+            </Text>
             <SelectMultiple
+              rowStyle={styles.select}
               items={this.props.actionQueue[0].handCards}
               renderLabel={renderLabel}
               selectedItems={this.state.selectedCards}
               onSelectionsChange={this.onSelectedCardsChange.bind(this)}
             />
-            <Button
-              style={{marginBottom: 2}}
-              title="Trash"
+            <TouchableHighlight
+              style={styles.button}
               onPress={
                 () => {
                   this.checkMaxSelected(4, 'chapel')
                 }
               }
             >
-            </Button>
-          </View>
+              <Text style={styles.buttonText}>Trash</Text>
+            </TouchableHighlight>
+          </ScrollView>
               )
       } else {
         <View>
-          <Text>There are no cards in your hand.</Text>
-          <Button
-            style={{marginBottom: 2}}
-            title="Ok"
+          <Text
+            style={styles.text}
+          >
+            There are no cards in your hand.
+          </Text>
+          <TouchableHighlight
+            style={styles.button}
             onPress={
               () => {this.props.resolveActionQueue()
               }
             }
           >
-          </Button>
+            <Text style={styles.buttonText}>Ok</Text>
+          </TouchableHighlight>
         </View>
       }
     },
     'harbinger': () => {
       if (this.props.actionQueue[0].discardCards.length > 0) {
         return (
-          <View>
+          <ScrollView>
+            <Text
+              style={styles.text}
+            >
+              You may select 1 card to move from your discard onto your deck.
+            </Text>
             <SelectMultiple
+              rowStyle={styles.select}
               items={this.props.actionQueue[0].discardCards}
               renderLabel={renderLabel}
               selectedItems={this.state.selectedCards}
               onSelectionsChange={this.onSelectedCardsChange.bind(this)}
             />
-            <Button
-              style={{marginBottom: 2}}
-              title="Add to Deck"
+            <TouchableHighlight
+              style={styles.button}
               onPress={
-                () => {
-                  this.checkMaxSelected(1, 'harbinger')
-                }
+                () => {this.checkMaxSelected(1, 'harbinger')}
               }
             >
-            </Button>
-          </View>
+              <Text style={styles.buttonText}>Add to Deck</Text>
+            </TouchableHighlight>
+          </ScrollView>
               )
       } else {
-        <View>
-          <Text>There are no cards in your discard pile.</Text>
-          <Button
-            style={{marginBottom: 2}}
-            title="Ok"
-            onPress={
-              () => {this.props.resolveActionQueue()}
-            }
-          >
-          </Button>
-        </View>
+        return (
+          <View>
+            <Text
+              style={styles.text}
+            >
+              There are no cards in your discard pile.
+            </Text>
+            <TouchableHighlight
+              style={styles.button}
+              onPress={
+                () => {this.props.resolveActionQueue()}
+              }
+            >
+              <Text style={styles.buttonText}>Ok</Text>
+            </TouchableHighlight>
+          </View>
+        )
       }
     }
+  }
+
+  titleCase(string) {
+    let newString = string.toLowerCase().split(' ').map((word) => {
+      return word.replace(word[0], word[0].toUpperCase());
+    });
+    return newString.join(' ')
   }
 
   render() {
     return (
       <View style={styles.overlay}>
+        <Text
+          style={styles.header}
+        >{this.titleCase(this.props.actionQueue[0].card)} Action</Text>
         { this.cardCallbacks[this.props.actionQueue[0].card]() }
       </View>
     )
@@ -182,18 +224,55 @@ export default class CallbackWindow extends Component {
 const styles = StyleSheet.create({
   overlay: {
     position: 'absolute',
-    top: responsiveHeight(9),
-    bottom: responsiveHeight(18),
+    top: responsiveHeight(5),
+    bottom: 0,
     left: 0,
     right: 0,
     borderWidth: 0,
     paddingBottom: responsiveHeight(5),
-    backgroundColor: 'orange',
+    backgroundColor: 'rgba(244, 161, 66, 0.95)',
+    alignItems: 'center',
   },
-  submit: {
-    backgroundColor: '#1d63d3',
+  text: {
+    fontSize: responsiveFontSize(2.5),
+    marginBottom: responsiveHeight(2),
+    textAlign: 'center',
+    marginLeft: responsiveHeight(3),
+    marginRight: responsiveHeight(3),
+  },
+  header: {
+    fontSize: responsiveFontSize(4),
+    marginBottom: responsiveHeight(3),
+    marginTop: responsiveHeight(2),
+  },
+  button: {
+    height: 26,
+    borderWidth: 1,
+    borderRadius: 8,
+    backgroundColor: '#2662bd',
+    borderColor: '#2662bd',
+    marginTop: responsiveWidth(2),
+    marginRight: responsiveWidth(1),
+    marginBottom: responsiveHeight(2),
+    width: responsiveWidth(33),
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1,
+    alignSelf: 'center',
+  },
+  buttonText: {
+    fontSize: responsiveFontSize(2),
     color: 'white',
-    padding: 15,
-    marginBottom: 2
+    alignSelf: 'center',
+    shadowOpacity: 0,
+    elevation: 0,
+  },
+  select: {
+    backgroundColor: 'transparent',
+    borderBottomWidth: 0,
+    left: responsiveWidth(30),
+    padding: 10,
   }
 })
